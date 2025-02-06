161fb1c57437989cd8373c76858764d2ec6e3a0f
Tito Ayorinde <101532533+titorinde@users.noreply.github.com>
2025-02-06 21:06:42 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+const menu = document.querySelector('#mobile-menu')
+const menuLinks = document.querySelector('.navbar_menu')
+
+menu.addEventListener('click', function () {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+});
+
+function alert() {
+    document.getElementById('alert').style = "display:block";
+    }
